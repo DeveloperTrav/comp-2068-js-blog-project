3eb118ff3521f0ddf2d5e8091c1c4a594ec28207
DeveloperTrav <Travis_Teerink@outlook.com>
2019-05-26 16:26:15 -0400

    Fix URL path issue

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const express = require(`express`);
 const app = express();
 
 app.get(`/`, (req, res) => {
-    res.sendFile(`Home.html`);
+    res.sendFile(`${__dirname}/Home.html`);
 });
 
 app.get(`/about`, (req, res) => {
-    res.sendFile(`About.html`);
+    res.sendFile(`${__dirname}/About.html`);
 });
 
 app.get(`/contact`, (req, res) => {
-    res.sendFile(`Contact.html`);
+    res.sendFile(`${__dirname}/Contact.html`);
 });
 
 app.listen((process.env.PORT || 4000), () => console.log("App is listening on port 4000."));
